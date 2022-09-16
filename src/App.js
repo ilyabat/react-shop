d2dd5ff7a2e2053624999f147171bc7b8892a9a1
ilyabanan <ibatuk53@gmail.com>
2022-09-16 22:57:48 +0300

    up.2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
 function App() {
   return (
     <div className="wrapper">
+      <div className="overlay">
+        <div className="drawer">
+          <div className="drawer__title">
+            Кошик
+          </div>
+          <div className="drawer__items">
+
+            <div className="drawer__item">
+              <img className="drawer__sneakers-img" src="/img/main/sneakers/01.jpg" alt="Sneakers" />
+              <div className="drawer__text">
+                <p>Чоловічі кросівки Nike Blazer Mid Suede</p>
+                <b>12 999грн.</b>
+              </div>
+              <img className="drawer__remove-img" src="/img/main/remove.svg" alt="Remove" />
+            </div>
+
+            <div className="drawer__item">
+              <img className="drawer__sneakers-img" src="/img/main/sneakers/01.jpg" alt="Sneakers" />
+              <div className="drawer__text">
+                <p>Чоловічі кросівки Nike Blazer Mid Suede</p>
+                <b>12 999грн.</b>
+              </div>
+              <img className="drawer__remove-img" src="/img/main/remove.svg" alt="Remove" />
+            </div>
+          </div>
+        </div>
+      </div>
+
       <header className="header">
         <div className="header__left">
-          <img src="/img/header/logo.png" />
+          <img src="/img/header/logo.png" alt="Logo" />
           <div className="header__info">
             <h3>BOOST SNEAKERS</h3>
             <p>Магазин найкращих кросівок</p>
@@ -11,15 +39,24 @@ function App() {
         </div>
 
         <ul className="header__right">
-          <li><img src="/img/header/cart.svg" /><span>1205грн.</span></li>
-          <li><img src="/img/header/user.svg" /></li>
+          <li><img src="/img/header/cart.svg" alt="Cart" /><span>1205грн.</span></li>
+          <li><img src="/img/header/user.svg" alt="User" /></li>
         </ul>
       </header>
       <div className="content">
-        <h1>Всі кросівки</h1>
+        <div className="content__block">
+          <h1>Всі кросівки</h1>
+          <div className="content__search">
+            <img src="/img/main/search.svg" alt="Search" />
+            <input type="text" placeholder="Пошук..." />
+          </div>
+        </div>
 
         <div className="card">
           <div className="card__block">
+            <div className="card__favorite">
+              <img src="/img/main/unlike.svg" alt="UnLike" />
+            </div>
             <img className="card__img" src="/img/main/sneakers/01.jpg" alt="Sneakers" />
             <p className="card__text">Чоловічі кросівки Nike Blazer Mid Suede</p>
             <div className="card__down">
