b434f2009841650d8e5cca784c1d076766a4c431
ilyabanan <ibatuk53@gmail.com>
2022-09-18 20:04:10 +0300

    update 0.4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+const arr = [
+  { title: 'Чоловічі кросівки Nike Blazer Mid Suede', price: 12999, imageUrl: "/img/main/sneakers/01.jpg" },
+  { title: 'Чоловічі кросівки Nike Air Max 270', price: 15199, imageUrl: "/img/main/sneakers/02.jpg" },
+  { title: 'Чоловічі кросівки Nike Blazer Mid Suede', price: 10199, imageUrl: "/img/main/sneakers/03.jpg" },
+  { title: 'Кросівки Puma X Aka Boku Future Rider', price: 8199, imageUrl: "/img/main/sneakers/04.jpg" },
+];
+
 function App() {
   return (
     <div className="wrapper">
@@ -17,67 +24,16 @@ function App() {
         </div>
 
         <div className="card">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {
+            arr.map((obj) => (
+              <Card
+                title={obj.title}
+                price={obj.price}
+                imageUrl={obj.imageUrl}
+              />
+            ))
+          }
 
-          <div className="card__block">
-            <div className="card__favorite">
-              <img src="/img/main/unlike.svg" alt="UnLike" />
-            </div>
-            <img className="card__img" src="/img/main/sneakers/01.jpg" alt="Sneakers" />
-            <p className="card__text">Чоловічі кросівки Nike Blazer Mid Suede</p>
-            <div className="card__down">
-              <div className="card__price">
-                <p>Ціна:</p>
-                <b>12 999грн.</b>
-              </div>
-              <button className="card__btn">
-                <img className="card__button-img" src="/img/main/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
-          <div className="card__block">
-            <img className="card__img" src="/img/main/sneakers/02.jpg" alt="Sneakers" />
-            <p className="card__text">Чоловічі кросівки Nike Blazer Mid Suede</p>
-            <div className="card__down">
-              <div className="card__price">
-                <p>Ціна:</p>
-                <b>12 999грн.</b>
-              </div>
-              <button className="card__btn">
-                <img className="card__button-img" src="/img/main/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
-          <div className="card__block">
-            <img className="card__img" src="/img/main/sneakers/03.jpg" alt="Sneakers" />
-            <p className="card__text">Чоловічі кросівки Nike Blazer Mid Suede</p>
-            <div className="card__down">
-              <div className="card__price">
-                <p>Ціна:</p>
-                <b>12 999грн.</b>
-              </div>
-              <button className="card__btn">
-                <img className="card__button-img" src="/img/main/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
-          <div className="card__block">
-            <img className="card__img" src="/img/main/sneakers/04.jpg" alt="Sneakers" />
-            <p className="card__text">Чоловічі кросівки Nike Blazer Mid Suede</p>
-            <div className="card__down">
-              <div className="card__price">
-                <p>Ціна:</p>
-                <b>12 999грн.</b>
-              </div>
-              <button className="card__btn">
-                <img className="card__button-img" src="/img/main/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
         </div>
 
 
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,22 @@
-function Card() {
+
+function Card(props) {
+
+    const onClickButton = () =>{
+        alert(props.title)
+    }
     return (
         <div className="card__block">
             <div className="card__favorite">
                 <img src="/img/main/unlike.svg" alt="UnLike" />
             </div>
-            <img className="card__img" src="/img/main/sneakers/01.jpg" alt="Sneakers" />
-            <p className="card__text">Чоловічі кросівки Nike Blazer Mid Suede</p>
+            <img className="card__img" src={props.imageUrl} alt="Sneakers" />
+            <p className="card__text">{props.title}</p>
             <div className="card__down">
                 <div className="card__price">
                     <p>Ціна:</p>
-                    <b>12 999грн.</b>
+                    <b>{props.price} грн</b>
                 </div>
-                <button className="card__btn">
+                <button className="card__btn" onClick={onClickButton}>
                     <img className="card__button-img" src="/img/main/plus.svg" alt="Plus" />
                 </button>
             </div>
